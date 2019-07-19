a4c851930dc80afb0bc004003639ee4bdf4d2e03
kroont339 <kroont339@gmail.com>
2019-07-19 14:05:34 -0400

    here is the delete rest service, and the archived document, and repositry

---
+++ src/main/java/com/collabera/controllers/ArchivedMessageController.java
@@ -0,0 +1,62 @@
+package com.collabera.controllers;
+
+import java.util.Date;
+import java.util.List;
+import java.util.Optional;
+
+import org.bson.types.ObjectId;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.collabera.models.ArchivedMessages;
+import com.collabera.models.Messages;
+import com.collabera.repositories.ArchivedMessagesRepository;
+
+@RestController
+public class ArchivedMessageController {
+	
+	@Autowired
+	ArchivedMessagesRepository ArchivedMessageService;
+	
+	@CrossOrigin(origins = "localhost:4200")
+	@GetMapping("/api/archived/messages")
+	public List<ArchivedMessages> getMessages(){
+		return ArchivedMessageService.findAll();
+	}
+	@CrossOrigin(origins = "localhost:4200")
+	@GetMapping("/api/archived/messages/{id}")
+	public ArchivedMessages getMessage(@PathVariable ObjectId id) {
+		Optional<ArchivedMessages> temp = ArchivedMessageService.findById(id);
+		if(temp.isPresent()) {
+			return temp.get();
+		}else {
+			return null;
+		}
+	}
+	
+	@CrossOrigin(origins = "http://localhost:4200")
+	@GetMapping("/api/archived/messages/byRoomId/{chatRoomId}")
+	public List<ArchivedMessages> getArchivedMessagesById(@PathVariable ObjectId chatRoomId){
+		List<ArchivedMessages> temp = ArchivedMessageService.findByChatRoomsIdOrderByTimestampAsc(chatRoomId);
+		if(temp.isEmpty()) {
+			return null;
+		}else {
+			return temp;
+		}
+	}
+	
+	@CrossOrigin(origins = "localhost:4200")
+	@PostMapping("/api/archived/messages")
+	public ResponseEntity<String> postArchivedMessage(@RequestBody ArchivedMessages message){
+		message.setTimeStamp(new Date());
+		ArchivedMessageService.save(message);
+		return ResponseEntity.ok("MESSAGE CREATED");
+	}
+
+}
---
+++ src/main/java/com/collabera/models/ArchivedMessages.java
@@ -0,0 +1,75 @@
+package com.collabera.models;
+
+import java.util.Date;
+
+import org.bson.types.ObjectId;
+import org.springframework.data.annotation.Id;
+
+public class ArchivedMessages {
+	
+
+	@Id
+	private ObjectId id;
+	private Users sender;
+	private String message;
+	private ChatRooms chatRooms;
+	private Date timestamp;
+	
+	public ArchivedMessages() {}
+
+	public ArchivedMessages(ObjectId id, Users sender, String message, ChatRooms chatRooms, Date timestamp) {
+		super();
+		this.id = id;
+		this.sender = sender;
+		this.message = message;
+		this.chatRooms = chatRooms;
+		this.timestamp = timestamp;
+	}
+
+	public String getId() {
+		return id.toHexString();
+	}
+
+	public void setId(ObjectId id) {
+		this.id = id;
+	}
+
+	public Users getSender() {
+		return sender;
+	}
+
+	public void setSender(Users sender) {
+		this.sender = sender;
+	}
+
+	public ChatRooms getChatRooms() {
+		return chatRooms;
+	}
+
+	public void setChatRooms(ChatRooms chatRooms) {
+		this.chatRooms = chatRooms;
+	}
+
+	public String getMessage() {
+		return message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+	
+	public Date getTimeStamp() {
+		return this.timestamp;
+	}
+	public void setTimeStamp(Date d) {
+		this.timestamp = d;
+	}
+
+	@Override
+	public String toString() {
+		return "Messages [id=" + id.toHexString() + ", sender=" + sender + ", message=" + message + ", chatRooms=" + chatRooms + ", timeStamp=" + timestamp + "]";
+	}
+
+
+	
+}
