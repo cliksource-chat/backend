41ee12479074142597093b5a9e5899cb7a49aab0
Said Washah <saidwashah@gmail.com>
2019-08-21 10:01:21 -0400

    Mongo archived update

--- src/main/java/com/collabera/archive_models/Messages_Archive.java
+++ src/main/java/com/collabera/archive_models/Messages_Archive.java
@@ -2,64 +2,55 @@ package com.collabera.archive_models;
 
 import java.util.Date;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
-import javax.persistence.Table;
+import org.bson.types.ObjectId;
+import org.springframework.data.annotation.Id;
 
-@Entity
-@Table(name="archived_messages")
+import com.collabera.models.ChatRooms;
+import com.collabera.models.Users;
+
 public class Messages_Archive {
 	
+
 	@Id
-	@GeneratedValue
-	private int id;
-	
-	@Column(name="mongoId")
-	private String mongoId;
-	
-	@Column(name="message")
+	private ObjectId id;
+	private Users sender;
 	private String message;
-	
-	@ManyToOne
-	@JoinColumn(name="sender")
-	private Users_Archive sender;
-	
-	@ManyToOne
-	@JoinColumn(name="chatrooms")
-	private ChatRooms_Archive chatroom;
-	
-	@Column(name="timestamp")
+	private ChatRooms chatRooms;
 	private Date timestamp;
 	
-	public Messages_Archive() {
-		
-	}
-	
-	public Messages_Archive(String mongoId, String message, Users_Archive sender, ChatRooms_Archive chatroom) {
-		this.mongoId = mongoId;
-		this.message = message;
+	public Messages_Archive() {}
+
+	public Messages_Archive(ObjectId id, Users sender, String message, ChatRooms chatRooms, Date timestamp) {
+		super();
+		this.id = id;
 		this.sender = sender;
-		this.chatroom = chatroom;
+		this.message = message;
+		this.chatRooms = chatRooms;
+		this.timestamp = timestamp;
 	}
 
-	public int getId() {
-		return id;
+	public String getId() {
+		return id.toHexString();
 	}
 
-	public void setId(int id) {
+	public void setId(ObjectId id) {
 		this.id = id;
 	}
 
-	public String getMongoId() {
-		return mongoId;
+	public Users getSender() {
+		return sender;
 	}
 
-	public void setMongoId(String mongoId) {
-		this.mongoId = mongoId;
+	public void setSender(Users sender) {
+		this.sender = sender;
+	}
+
+	public ChatRooms getChatRooms() {
+		return chatRooms;
+	}
+
+	public void setChatRooms(ChatRooms chatRooms) {
+		this.chatRooms = chatRooms;
 	}
 
 	public String getMessage() {
@@ -69,34 +60,17 @@ public class Messages_Archive {
 	public void setMessage(String message) {
 		this.message = message;
 	}
-
-	public Users_Archive getSender() {
-		return sender;
-	}
-
-	public void setSender(Users_Archive sender) {
-		this.sender = sender;
-	}
-
-	public ChatRooms_Archive getChatroom() {
-		return chatroom;
-	}
-
-	public void setChatroom(ChatRooms_Archive chatroom) {
-		this.chatroom = chatroom;
-	}
-
-	public Date getTimestamp() {
-		return timestamp;
-	}
-
-	public void setTimestamp(Date timestamp) {
-		this.timestamp = timestamp;
-	}
 	
-	
-	
-	
-	
+	public Date getTimeStamp() {
+		return this.timestamp;
+	}
+	public void setTimeStamp(Date d) {
+		this.timestamp = d;
+	}
+
+	@Override
+	public String toString() {
+		return "Messages [id=" + id.toHexString() + ", sender=" + sender + ", message=" + message + ", chatRooms=" + chatRooms + ", timeStamp=" + timestamp + "]";
+	}
 
 }
